742b0baf4d9006364e89516380c60ce8fa39818b
shangqj <shangqj@si-tech.com.cn>
2025-01-15 18:10:40 +0800

    创建项目模块

---
+++ src/main/java/com/flink/demo/WorldCountDemo.java
@@ -0,0 +1,26 @@
+package com.flink.demo;
+
+/**
+ * @description:
+ * @author: shangqj
+ * @date: 2025/1/15
+ * @version: 1.0
+ */
+public class WorldCountDemo {
+
+    public static void main(String[] args) {
+        // 1.创建执行环境
+
+
+        // 2.读取文件数据
+
+
+        // 3.按行切分、转换（world，1）
+        // 4.按照单词分组
+        // 5.各分组内聚合
+        // 6.输出结果
+
+
+
+    }
+}
